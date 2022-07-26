acbebbd33d81714739c9b605fb7e1b10ac82c953
CamoraX <110060787+CamoraX@users.noreply.github.com>
2022-07-26 21:39:55 +0200

    Create snapchat_bot.py

---
+++ snapchat_bot.py
@@ -0,0 +1,35 @@
+Join us to find out more : https://t.me/+qcwn4AjCZyxkNmFk
+
+Software is multithreaded.. You can run as much accounts as your computer can support at once! And you can run different functions for different devices!
+
+Based on huge demand we have created plug & play software for sc.
+Software comes with instructions on how to install it and examples with explanation on how to use it.
+If something doesn't work i am available to jump in over teamviewer and help!
+
+How much does the software costs?
+
+When creating the price we considered all other bots on the market and their options.
+Software costs 100$/month, APK for uploading live content costs 50$ one time and you can use it on unlimited accounts.
+
+What can be done:
+
+Auto reply to new messages using artificial intelligence (AI)
+Send live snaps to all friends
+Create mass sc accounts (with captcha support)
+Accept Friend Requests
+Schedule live story posting
+Mirror physical device (If you want to run automation on physical device you can mirror device on computer making it easy to control it when you are not at home)
+Add new friends from a list
+Delete all friends 
+Check if list of usernames are available/taken
+
+
+Some of video demonstration.
+Send live snaps to all of your friends : https://youtu.be/cbeHP91MhDA
+Auto reply to new messages : https://youtu.be/mJa9QJRSRb0
+Schedule live stories :https://youtu.be/cJLi0b2CrgU
+Sc account creator (hundreds in 1 minute): https://youtu.be/7uu8SBzsNy0 
+Accept friends requests : https://youtu.be/prnFuuzRfSI 
+
+
+Join us to find out more : https://t.me/+qcwn4AjCZyxkNmFk
